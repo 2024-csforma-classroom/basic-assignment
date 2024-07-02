4cbf831d346c4fd30e9f20e4fe5e9383ad2a29dc
DCPetty <github@patton-petty.net>
2024-07-02 07:13:57 -0400

    Added comments.

--- test/AssignmentTest.java
+++ test/AssignmentTest.java
@@ -13,22 +13,29 @@ import org.junit.Test;
 
 public class AssignmentTest {
 
+    private String toString;
 
     @Before
     public void setUp() {
+        // TODO: set up any test data before each test
+        toString = "Assignment()";
     }
 
     @After
     public void tearDown() {
+        // TODO: tear down any test data after each test
+        toString = null;
     }
 
     @Test
     public void testToString() {
+        // Log test name to console.
         System.out.printf("%s\n", Feedback.formatBanner("testToString"));
         Assignment assignment = new Assignment();
-        String toString = "Assignment()";
+        // Log student feedback to console.
         System.out.printf("\"%s\" \u225f \"%s\"\n",
             toString, assignment.toString());
+        // Use Assert to test method behaviors (including failure message).
         Assert.assertEquals(String.format("\"%s\" != \"%s\"", 
             toString, assignment.toString()),
             toString, assignment.toString());
--- test/OtherTest.java
+++ test/OtherTest.java
@@ -6,31 +6,39 @@
  * @author David C. Petty // http://j.mp/psb_david_petty
  */
 
-import org.junit.After;
-import org.junit.Assert;
-import org.junit.Before;
-import org.junit.Test;
-
-public class OtherTest {
-
-
-    @Before
-    public void setUp() {
-    }
-
-    @After
-    public void tearDown() {
-    }
-
-    @Test
-    public void testRepr() {
-        System.out.printf("%s\n", Feedback.formatBanner("testRepr"));
-        Assignment assignment = new Assignment();
-        String repr = "new Assignment()";
-        System.out.printf("\"%s\" \u225f \"%s\"\n",
-            repr, assignment.repr());
-        Assert.assertEquals(String.format("\"%s\" != \"%s\"", 
-            repr, assignment.repr()),
-            repr, assignment.repr());
-    }
-}
+ import org.junit.After;
+ import org.junit.Assert;
+ import org.junit.Before;
+ import org.junit.Test;
+ 
+ public class OtherTest {
+ 
+     private String repr;
+ 
+     @Before
+     public void setUp() {
+         // TODO: set up any test data before each test
+         repr = "new Assignment()";
+     }
+ 
+     @After
+     public void tearDown() {
+         // TODO: tear down any test data after each test
+         repr = null;
+     }
+ 
+     @Test
+     public void testRepr() {
+         // Log test name to console.
+         System.out.printf("%s\n", Feedback.formatBanner("testRepr"));
+         Assignment assignment = new Assignment();
+         // Log student feedback to console.
+         System.out.printf("\"%s\" \u225f \"%s\"\n",
+             repr, assignment.repr());
+         // Use Assert to test method behaviors (including failure message).
+         Assert.assertEquals(String.format("\"%s\" != \"%s\"", 
+             repr, assignment.repr()),
+             repr, assignment.repr());
+     }
+ }
+ 
